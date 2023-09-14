5748bab4843d2cf3e243b1d46a4c27a496169617
orfeu <francgbsj1995@gmail.com>
2023-09-13 22:21:12 -0300

    eval delete

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,10 +4,9 @@ function addToInput(value) {
 }
 
 function calculate() {
-    
     const input = document.getElementById('inputCalc');
     try {
-        const result = eval(input.value);
+        const result = new Function('return ' + input.value)();
         input.value = result;
     } catch (error) {
         input.value = 'Erro';
@@ -17,7 +16,7 @@ function calculate() {
 function calculateSquare() {
     const input = document.getElementById('inputCalc');
     try {
-        const squareValue = eval(input.value);
+        const squareValue = new Function('return ' + input.value)();
         const result = Math.pow(squareValue, 2);
         input.value = result;
     } catch (error) {
@@ -28,8 +27,8 @@ function calculateSquare() {
 function calculateRoot() {
     const input = document.getElementById('inputCalc');
     try {
-        const inputValue = eval(input.value);
-        const result = Math.sqrt(inputValue)
+        const inputValue = new Function('return ' + input.value)();
+        const result = Math.sqrt(inputValue);
         input.value = result;
     } catch (error) {
         input.value = 'Erro';
