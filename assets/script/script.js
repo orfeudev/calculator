931dff5e41fe142183fa22b86d5d11ab836ddc97
orfeu <francgbsj1995@gmail.com>
2023-09-17 12:42:27 -0300

    Support for keyboards has been added.

--- assets/script/script.js
+++ assets/script/script.js
@@ -39,3 +39,43 @@ function clearInput() {
     const input = document.getElementById('inputCalc')
     input.value = '';
 }
+
+const charToOperation = {
+  42: "*",
+  43: "+",
+  45: "-",
+  47: "/",
+};
+
+const isNum = (charCode) => {
+  return charCode > 47 && charCode < 58;
+};
+
+const isDot = (charCode) => {
+  return charCode === 46;
+};
+
+const isCalculate = (charCode) => {
+  return charCode === 13;
+};
+
+window.addEventListener("keypress", function (event) {
+  var charCode = event.which ? event.which : event.keyCode;
+
+  event.preventDefault();
+  if (isNum(charCode)) {
+    addToInput(parseInt(event.key, 10));
+  }
+
+  if (isDot(charCode)) {
+    addToInput(".");
+  }
+
+  if (charToOperation[charCode]) {
+    addToInput(charToOperation[charCode]);
+  }
+
+  if (isCalculate(charCode)) {
+    calculate();
+  }
+});
